8da78a29eb9cee507f5e0402869a58e2b0e0537c
MSI\sims7 <nsi242@uky.edu>
2025-03-31 10:06:14 -0400

    modified:   src/httpClient.js

--- src/httpClient.js
+++ src/httpClient.js
@@ -4,7 +4,7 @@ import { Image, StyleSheet, Text, View, Button, Alert, TextInput} from 'react-na
 //create account function 
 export const createAccount = async (Name, username, email, password) => {
     try{
-        const response = await fetch('http://10.0.0.43:4000/users/createAccount', {
+        const response = await fetch('http://10.47.93.235:4000/users/createAccount', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -13,9 +13,11 @@ export const createAccount = async (Name, username, email, password) => {
 
         });
 
+        
+
         if (response.ok) {
             const data = await response.json();
-            Alert.alert('Success', `Account created: ${data.username}`); // Display data in alert
+            Alert.alert('Success', `Account created`); // Display data in alert
           } else {
             Alert.alert('Error', 'Failed to create account');
           }
@@ -49,7 +51,7 @@ export const testGet = async () => {
 //Auth packager
 export const auth = async (username, password) => {
     try{
-        const response = await fetch('http://10.0.0.43:4000/auth/', {
+        const response = await fetch('http://10.47.93.235:4000/auth/', {
             method: 'POST',
             credentials: 'include', 
             headers: {
@@ -91,7 +93,7 @@ export const educationalMaterials = async (pdfName, infoRequested) => {
     // const pdfName = 'Taking Care of Your Flushable Drain Tubes';
     //  const infoRequested = 'post-surgery';
     try{
-        const response = await fetch('http://10.0.0.43:4000/materials/educational', {
+        const response = await fetch('http://10.47.93.235:4000/materials/educational', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -125,7 +127,7 @@ export const getProfileInfo = async () => {
     const pdfName = 'Taking Care of Your Flushable Drain Tubes';
     const infoRequested = 'post-surgery';
     try{
-        const response = await fetch('http://10.0.0.43:4000/profile/retrieve', {
+        const response = await fetch('http://10.47.93.235:4000/profile/retrieve', {
             method: 'POST',
             credentials: true,
             headers: {
