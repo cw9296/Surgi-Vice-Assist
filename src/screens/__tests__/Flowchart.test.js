3b265b56e110c77f8563728e51d3f7711e447d10
AJ Wyatt <wyattjalex@gmail.com>
2025-04-06 21:05:47 -0400

    Update Flowchart.test.js

---
+++ src/screens/__tests__/Flowchart.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react-native'
+import FlowchartScreen from './src/screens/Flowchart.js';
+
+test('Dependencies are defined', () => {
+
+    expect(FlowchartScreen).toBeDefined();
+
+  });
+/*
+test('SvgAvatar renders correctly', ()=>{
+    expect(render(<SvgAvatar />)).toBeDefined();
+});
+
+test('AvatarScreen renders correctly', ()=>{
+    expect(render(<AvatarScreen />)).toBeDefined();
+});
+test('All components render', async ()=>{
+    render(<SvgAvatar />);
+    //get the components
+    const legs = screen.queryByTestId('Legs');
+    expect(legs).toBeDefined();
+    const head = screen.queryByTestId('Head');
+    expect(head).toBeDefined();
+    const lf_arm = screen.queryByTestId('Lf_Arm');
+    expect(lf_arm).toBeDefined();
+    const rt_arm = screen.queryByTestId('Rt_Arm');
+    expect(rt_arm).toBeDefined();
+    const chest = screen.queryByTestId('Chest');
+    expect(chest).toBeDefined();
+});
+test('Entire SVG renders on screen', ()=>{
+    render(<AvatarScreen />);
+    const svg = screen.queryByTestId('SVG');
+    expect(svg).toBeDefined();
+});*/
