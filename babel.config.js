5879307e6057f80f57d0eee4f749b0af2e5725b7
AJ Wyatt <wyattjalex@gmail.com>
2025-04-06 21:24:33 -0400

    added configs for babel and jest
    trying this out because it seemed like it was on caroline's branch and we were working on it together earlier. we are struggling to get jest to work at all unfortunately

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = function (api) {
+  api.cache(true);
+    return {
+      presets: ["module:@react-native/babel-preset"],
+      plugins: [
+
+        "@babel/plugin-transform-flow-strip-types",
+      ["@babel/plugin-transform-private-methods",
+        {
+          "loose": true
+        }],
+
+      ]
+    };
+  };
+  
